ee8bc707ef240a04f7bcd6c98ebc407d76431da0
daniel chand <daniel@ashrayconsulting.com>
2025-12-02 18:41:05 +0545

    update config

--- ui/src/config/env.ts
+++ ui/src/config/env.ts
@@ -1,8 +1,6 @@
 export const config = {
-  BASE_URL: import.meta.env.VITE_BASE_URL,
-  MODE: import.meta.env.VITE_MODE,
-  DEBUG: import.meta.env.VITE_MODE === "true",
+  BASE_URL:
+    import.meta.env.VITE_IS_PROD == 1 ? "http://localhost:2000/api" : "/api",
   TIMEOUT: import.meta.env.VITE_TIMEOUT,
-  IS_UNDER_MAINTENANCE: import.meta.env.VITE_IS_UNDER_MAINTENANCE === "1",
   TOKEN_STORAGE_KEY: import.meta.env.VITE_TOKEN_STORAGE_KEY,
 };
