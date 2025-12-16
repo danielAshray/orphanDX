32a7b39cac4b399258db44fb55cc2d775010eecb
Adarsh Gharti <adarsh.gcdev@gmail.comm>
2025-12-16 11:32:39 +0545

    hart lab logic added

--- server/src/controllers/patientRecommendation.controller.ts
+++ server/src/controllers/patientRecommendation.controller.ts
@@ -18,7 +18,7 @@ export interface Diagnosis {
   icd10: String;
 }
 
-const qualifyingIcd10Codes: string[] = [
+const owlIcd10Codes: string[] = [
   "E11.9",
   "E11.65",
   "R73.01",
@@ -54,6 +54,55 @@ const qualifyingIcd10Codes: string[] = [
   "K75.81",
   "R74.8",
   "R94.5",
+];
+
+const hartLabIcd10Codes: string[] = [
+  "I20.9",
+  "I20.8",
+  "R07.9",
+  "R07.89",
+  "R06.02",
+  "R07.2",
+  "R12",
+  "R53.81",
+  "M79.602",
+  "R68.84",
+  "R00.2",
+  "R42",
+  "I25.119",
+  "I70.209",
+  "I25.10",
+  "I25.110",
+  "I25.111",
+  "I25.5",
+  "I25.702",
+  "I10",
+  "Z82.41",
+  "Z82.49",
+  "E78.5",
+  "E78.2",
+  "E11.9",
+  "E11.8",
+  "E11.22",
+  "E10.9",
+  "E10.8",
+  "N18.9",
+  "N18.1",
+  "N18.2",
+  "N18.30",
+  "N18.4",
+  "N18.5",
+  "E88.81",
+  "E66.9",
+  "I65.21",
+  "I65.22",
+  "I65.23",
+  "F17.210",
+  "F17.218",
+  "F17.219",
+  "R94.39",
+  "R94.31",
+  "I73.9",
 ];
 
 const calculateAge = (dob: Date | string) => {
@@ -98,20 +147,31 @@ export const recommendPatient = async (
 
     const lab = await prisma.lab.findFirst({ where: { id: labId } });
     throwNotFound(lab, "Lab");
-    const labMatch = lab!.name === "Luxor Scientific Lab";
+    const owlLabMatch = lab!.name === "Luxor Scientific Lab";
 
     const age = calculateAge(patient!.dob);
-    if (labMatch && age < 18) {
+    if (owlLabMatch && age < 18) {
       return res.status(422).json({
         message:
           "Patient must be at least 18 years of age to qualify for the test.",
       });
     }
 
-    const status = patient?.patientDiagnoses.some((diagnosis) =>
-      qualifyingIcd10Codes.includes(diagnosis.diagnosis.icd10)
+    const foundOwlCode = patient?.patientDiagnoses.some((diagnosis) =>
+      owlIcd10Codes.includes(diagnosis.diagnosis.icd10)
     );
-    if (labMatch && !status) {
+    if (owlLabMatch && !foundOwlCode) {
+      return res.status(422).json({
+        message: "Patient does not meet the stated diagnosis code.",
+      });
+    }
+
+    const hartLabMatch = lab!.name === "HART Lab";
+    const foundHartLabCode = patient?.patientDiagnoses.some((diagnosis) =>
+      hartLabIcd10Codes.includes(diagnosis.diagnosis.icd10)
+    );
+
+    if (hartLabMatch && !foundHartLabCode) {
       return res.status(422).json({
         message: "Patient does not meet the stated diagnosis code.",
       });
@@ -167,20 +227,31 @@ export const editPatientRecommendation = async (
 
     const lab = await prisma.lab.findFirst({ where: { id: labId } });
     throwNotFound(lab, "Lab");
-    const labMatch = lab!.name === "Luxor Scientific Lab";
+    const owlLabMatch = lab!.name === "Luxor Scientific Lab";
 
     const age = calculateAge(patient!.dob);
-    if (labMatch && age < 18) {
+    if (owlLabMatch && age < 18) {
       return res.status(422).json({
         message:
           "Patient must be at least 18 years of age to qualify for the test.",
       });
     }
 
-    const status = patient?.patientDiagnoses.some((diagnosis) =>
-      qualifyingIcd10Codes.includes(diagnosis.diagnosis.icd10)
+    const hartLabMatch = lab!.name === "HART Lab";
+    const foundHartLabCode = patient?.patientDiagnoses.some((diagnosis) =>
+      hartLabIcd10Codes.includes(diagnosis.diagnosis.icd10)
     );
-    if (labMatch && !status) {
+
+    if (hartLabMatch && !foundHartLabCode) {
+      return res.status(422).json({
+        message: "Patient does not meet the stated diagnosis code.",
+      });
+    }
+
+    const foundOwlCode = patient?.patientDiagnoses.some((diagnosis) =>
+      owlIcd10Codes.includes(diagnosis.diagnosis.icd10)
+    );
+    if (owlLabMatch && !foundOwlCode) {
       return res.status(422).json({
         message: "Patient does not meet the stated diagnosis code.",
       });
