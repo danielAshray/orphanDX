eca88fc5a428373fb52918ecc747aa36d5bbcf95
daniel chand <daniel@ashrayconsulting.com>
2025-12-21 12:00:38 +0545

    remove unused api

--- server/src/controllers/patient.controller.ts
+++ server/src/controllers/patient.controller.ts
@@ -3,62 +3,6 @@ import { prisma } from "../lib/prisma";
 import { sendResponse } from "../utils/responseService";
 import { ApiError } from "../utils/apiService";
 
-export const createPatient = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const {
-      firstName,
-      lastName,
-      mrn,
-      dateOfBirth,
-      gender,
-      phone,
-      email,
-      insurance,
-      facilityId,
-    } = req.body;
-
-    const patient = await prisma.$transaction(async (tx) => {
-      const newPatient = await tx.patient.create({
-        data: {
-          firstName,
-          lastName,
-          mrn,
-          dateOfBirth,
-          gender,
-          phone,
-          email,
-          facilityId,
-        },
-      });
-
-      if (insurance) {
-        await tx.insurance.create({
-          data: {
-            patientId: newPatient.id,
-            provider: insurance.provider,
-            plan: insurance.plan,
-          },
-        });
-      }
-
-      return newPatient;
-    });
-
-    sendResponse(res, {
-      success: true,
-      code: 200,
-      message: "Patient created successfully",
-      data: patient,
-    });
-  } catch (error: any) {
-    next(ApiError.internal(undefined, error));
-  }
-};
-
 export const fetchPatientDetails = async (
   req: Request,
   res: Response,
