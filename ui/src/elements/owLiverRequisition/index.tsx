f7c89111f131a573d8755825f340fc88a38a7f96
daniel chand <daniel@ashrayconsulting.com>
2025-12-24 20:49:09 +0545

    update pdf path

--- ui/src/elements/owLiverRequisition/index.tsx
+++ ui/src/elements/owLiverRequisition/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
-import templatePdf from "@/templates/OW_Liver_Requisition.pdf";
+import templatePdf from "./OW_Liver_Requisition.pdf";
 import { Loader } from "lucide-react";
 
 type FormValues = {
