61d1f60077dd5c92836f651fcecbfa5d726ec1f1
daniel chand <daniel@ashrayconsulting.com>
2025-12-24 19:58:19 +0545

    update base api

--- ui/src/config/env.ts
+++ ui/src/config/env.ts
@@ -1,5 +1,4 @@
 export const config = {
-  BASE_URL:
-    import.meta.env.VITE_IS_PROD === 1 ? "/api" : import.meta.env.VITE_BASE_URL,
+  BASE_URL: "/api",
   TIMEOUT: import.meta.env.VITE_TIMEOUT,
 };
